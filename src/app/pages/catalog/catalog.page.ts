dec1cac22ac581d6e0238f005106ec3816b8c988
VladimirMaestro <102423850+VladimirMaestro@users.noreply.github.com>
2025-05-02 23:59:01 +0300

    add filters price MaxMin

--- src/app/pages/catalog/catalog.page.ts
+++ src/app/pages/catalog/catalog.page.ts
@@ -2,7 +2,12 @@ import { Component, inject } from '@angular/core';
 import { Product } from '../../models/product';
 import { AppStore } from '../../store/app.store';
 import { AppState } from '../../models/app-state';
+import { ProductFilters } from '../../models/products-filters';
 
+interface PriceInputStatus {
+  value: string,
+  cursor: number
+}
 @Component({
   standalone: false,
   templateUrl: 'catalog.page.html',
@@ -10,7 +15,18 @@ import { AppState } from '../../models/app-state';
 })
 export class CatalogPage {
   private appStore: AppStore = inject(AppStore);
+  public priceMin: string = '';
+  public priceMax: string = '';
+  private prevValues: { [key: string]: PriceInputStatus } = {};
   public products: Product[] = [];
+
+  applyPriceMinMax(): void {
+    const filters: Partial<ProductFilters> = {
+      priceMin: Number(this.priceMin),
+      priceMax: Number(this.priceMax),
+    };
+    this.appStore.updateProductFilters(filters);
+  }
 
   ngOnInit(): void {
     this.appStore.state$.subscribe((state: AppState) => {
@@ -18,7 +34,25 @@ export class CatalogPage {
     })
   }
 
-  removeDecimals(input: HTMLInputElement): void {
-    input.value = input.value.replace(/\.|,|-/g, '');
+  onBeforeInput(key: string, input: HTMLInputElement): void {
+    this.prevValues[key] = {
+      value: input.value,
+      cursor: input.selectionStart!
+    };
+  }
+
+  onPriceRangeInput(key: string, input: HTMLInputElement): void {
+    if (!input.value) return;
+    if (!this.isInteger(input.value)) {
+      const prev: PriceInputStatus = this.prevValues[key];
+      input.value = prev.value;
+      input.setSelectionRange(prev.cursor, prev.cursor);
+    }
+  }
+
+  private isInteger(str: string): boolean {
+    return /^(0|[1-9]\d*)$/.test(str);
   }
 }
+
+
